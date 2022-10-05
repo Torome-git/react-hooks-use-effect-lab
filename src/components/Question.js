b0a376100582b53e704481fcf25fb3fc674ffbd7
soila.torome <soila.torome@student.moringaschool.com>
2022-10-05 15:53:18 +0300

    Completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,6 +4,18 @@ function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining(() => timeRemaining - 1);
+    }, 1000);
+
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+
+    return () => clearTimeout(timer);
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
